b88ef466fc909c2acf9a48d1d33e32164e1bfcd3
AnansiOmega <ignasbutautas7@gmail.com>
2020-12-14 15:57:24 -0600

    adjustments for launch

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
-    <Router>
+    <Router basename='/KeyboardWarrior'>
       <React.StrictMode>
         <App />
       </React.StrictMode>
